977d54e1877d000b784da94cdabc98094ecea5dc
John Peterson <nxh7@duck.com>
2022-12-11 21:06:45 +0330

    Add json view to adminpanel controller

--- app/controllers/adminpanel.php
+++ app/controllers/adminpanel.php
@@ -5,7 +5,6 @@ class AdminPanel extends Controller{
     public function update_month(...$params){
         $model = $this->model('admin');
         $month = $params[0];
-        $isChecked = $model->checkEntryInputs($month);
         $update = $model->updateMonth($params[0]);
     }
 
@@ -13,60 +12,117 @@ class AdminPanel extends Controller{
         $model = $this->model('admin');
         if ($params[0]){
             $update = $model->updateMonth($params[0]);
-            echo $update;
+            $result = [
+                "status" => "success",
+                "message" => "successfuly changed month"
+            ];
+            $this->view("json", $result);
         }
     }
 
     public function change_privilege(...$params){
         $model = $this->model('admin');
+        $result = [];
         if ($params[0] && $params[1]){
             $change = $model->changePrivilege($params[0], $params[1]);
-            echo $change;
+            if ($change == OK){
+                $result = [
+                    "status" => "success"
+                ];
+            }else {
+                $result = [
+                    "status" => "error"
+                ];
+            }
+            $this->view("json", $result);
         }
     }
 
     public function get_members(...$params){
         $model = $this->model('admin');
         if ($params[0] && $params[1]){
-            //TODO here
+            echo "There are two params : {$params[0]}, {$params[1]}";
+        }else{
+            echo "there is nothing here.";
         }
     }
 
     public function add_charge_to_user(){
-        $username = $_POST["username"];
-        $targetUsername = $_POST["target_username"];
-        $year = $_POST["year"];
-        $month = $_POST["month"];
+        $postParams = $_POST;
         $model = $this->model('admin');
-        $isChecked = $model->checkEntryElements($username, $year, $month);
-        echo $isChecked;
+        $isChecked = Validator::validateElements($postParams, [
+            'username',
+            'target_username',
+            'year',
+            'month'
+        ]);
+        if ($isChecked){
+            $username = $_POST["username"];
+            $targetUsername = $_POST["target_username"];
+            $year = $_POST["year"];
+            $month = $_POST["month"];
+            $result = [
+                "status" => "success",
+                "message" => "charge added to $targetUsername"
+            ];
+        }else{
+            $result = [
+                "status" => "error",
+                "message" => "something went wrong"
+            ];
+        }
+        $this->view("json", $result);
     }
 
     public function remove_charge_from_user(){
         $username = $_POST["username"];
         $targetUsername = $_POST["target_username"];
+        $postData = $_POST;
+        $result = [];
         $year = $_POST["year"];
         $month = $_POST["month"];
         $model = $this->model('admin');
-        if (isset($username) && isset($targetUsername) &&
-            isset($year) && isset($month)
-        ){
+        $validateRequiredElements = Validator::validateElements($postData, [
+            'username',
+            'target_username',
+            'year',
+            'month'
+        ]);
+        if ($validateRequiredElements){
             $removeCharge = $model->removeChargeOfTheUser($targetUsername, $year, $month);
             if ($removeCharge == OK){
-                $result = ['start' => 'end'];
-                $this->view("json", $result);
+                $result = [
+                    "status" => "success",
+                    "message" => "charge removed"
+                ];
+            }else {
+                $result = [
+                    "status" => "error",
+                    "message" => "charge didn't remove"
+                ];
             }
+        }else{
+            $result = [
+                "status" => "error",
+                "message" => "you haven't provided needed parameters"
+            ];
         }
+        $this->view("json", $result);
     }
 
     public function add_mojtama_rules(){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $rule = $_POST["rule"];
+        $postParams = $_POST;
         $model = $this->model('admin');
-        $isChecked = $model->checkEntryInputs($username, $password, $rule);
+        $isChecked = Validator::validateElements($postParams, [
+            'username',
+            'password',
+            'rule'
+        ]);
         $result = [];
-        if ($isChecked == OK){
+        if ($isChecked){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $rule = $_POST["rule"];
             $isOK = $model->addMojtamaRules($rule);
             if ($isOK){
                 $result = [
@@ -83,28 +139,87 @@ class AdminPanel extends Controller{
                 "status" => "error",
                 "message" => "You haven't specified inputs"
             ];
-
         }
         $this->view("json", $result);
     }
 
     public function add_mojtama_financial_status(){
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $financialStatusByJson = $_POST["financial_json"];
-
-        $model = $this->model('admin');
-        $isChecked = $model->checkEntryInputs($username, $password, $financialStatus);
-        if ($isChecked){
-            $model->addMojtamaFinancialStatus($financialStatusByJson);
-            $result = [
-                "status" => "success",
-                "message" => "financial status added"
-            ];
+        $postParams = $_POST;
+        $validateRequiredParameters = Validator::validateElements($postParams, [
+            'username',
+            'password',
+            'financial_json'
+        ]);
+        if ($validateRequiredParameters){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $financialStatusByJson = $_POST["financial_json"];
+            $model = $this->model('admin');
+            $isChecked = $model->checkEntryInputs($username, $password, $financialStatus);
+            if ($isChecked){
+                $model->addMojtamaFinancialStatus($financialStatusByJson);
+                $result = [
+                    "status" => "success",
+                    "message" => "financial status added"
+                ];
+            }else{
+                $result = [
+                    "status" => "error",
+                    "message" => "you haven't specified inputs"
+                ];
+            }
         }else{
             $result = [
                 "status" => "error",
-                "message" => "You haven't specified inputs"
+                "message" => "you haven't provided required parameters"
+            ];
+        }
+        $this->view("json", $result);
+    }
+
+    public function remove_user($targetUsername=''){
+        $result = [];
+        $model = $this->model('admin');
+        if (!empty($targetUsername)){
+            $postParams = $_POST;
+            $validateRequiredParams = Validator::validateElements($postParams, [
+                'username',
+                'password'
+            ]);
+            unset($postParams);
+            if ($validateRequiredParams){
+                $adminUsername = $_POST["username"];
+                $adminPassword = $_POST["password"];
+                $isAdmin = $model->checkFullAdmin($adminUsername, $adminPassword);
+                if ($isAdmin){
+                    $ans = $model->removeUser($targetUsername);
+                    if ($ans){
+                        $result = [
+                            "status" => "success",
+                            "message" => "user removed from database"
+                        ];
+                    }else{
+                        $result = [
+                            "status" => "error",
+                            "message" => "user didn't removed"
+                        ];
+                    }
+                }else{
+                    $result = [
+                        "status" => "error",
+                        "message" => "permission denied"
+                    ];
+                }
+            }else{
+                $result = [
+                    "status" => "error",
+                    "message" => "you haven't provided required params"
+                ];
+            }
+        }else{
+            $result = [
+                "status" => "error",
+                "message" => "you haven't provided target username"
             ];
         }
         $this->view("json", $result);
