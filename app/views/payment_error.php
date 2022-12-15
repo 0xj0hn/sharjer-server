1b9f759a722eec773fe88e0479cc4a4968617705
John Peterson <nxh7@duck.com>
2022-12-15 13:46:17 +0330

    Add error screen for payment failure

---
+++ app/views/payment_error.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+    <style><?php include "./app/style.css"?></style>
+</head>
+<body>
+    <h1 class="center">پرداخت با شکست مواجه شد.</h1>
+    <h2 class="center"><?=$data[0]?></h2>
+</body>
+
+</html>
